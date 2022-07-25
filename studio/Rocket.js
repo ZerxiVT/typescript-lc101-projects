9e279a70e54201ffef68e8bf8737c136be15fdb6
Kaz <kikainvr@gmail.com>
2022-07-25 18:57:39 -0500

    finished studio

---
+++ studio/Rocket.js
@@ -0,0 +1,59 @@
+"use strict";
+exports.__esModule = true;
+exports.Rocket = void 0;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.cargoItems = [];
+        this.astronauts = [];
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    ;
+    Rocket.prototype.sumMass = function (items) {
+        var sumMass = 0;
+        for (var i = 0; i < items.length; i++) {
+            sumMass += items[i].massKg;
+        }
+        ;
+        return sumMass;
+    };
+    ;
+    Rocket.prototype.currentMassKg = function () {
+        var astronautMass = this.sumMass(this.astronauts);
+        var cargoMass = this.sumMass(this.cargoItems);
+        return cargoMass += astronautMass;
+    };
+    ;
+    Rocket.prototype.canAdd = function (item) {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo) === true) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
+        ;
+    };
+    ;
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        if (this.canAdd(astronaut) === true) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    ;
+    return Rocket;
+}());
+exports.Rocket = Rocket;
+;
